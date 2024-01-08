fedaec458005c36dde108581788e2f21b4858603
Deepaabishek <deepaabishek1212@gmail.com>
2024-01-08 12:39:54 +0530

    completed Number_Palindrome and created a method for Number_Reverse

--- Back_End-Development/Java/Java_Exercise-Programs/src/Number_Reverse.java
+++ Back_End-Development/Java/Java_Exercise-Programs/src/Number_Reverse.java
@@ -5,14 +5,19 @@ public class Number_Reverse {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number: ");
         long num = input.nextLong();
-        int ans = 0;
         System.out.println("Input Number: "+num);
-        while (num > 0) {
-            int rem = (int)num% 10;
+        long ans = numberReverse(num);
+        System.out.println("Reversed Number: "+ans);
+        input.close();
+    }
+
+    public static long numberReverse(long num){
+        long ans = 0;
+            while (num > 0) {
+            int rem = (int)num % 10;
             ans = ans * 10 + rem;
             num /= 10;
         }
-        System.out.println("Reversed Number: "+ans);
-        input.close();
+          return ans;
     }
 }
